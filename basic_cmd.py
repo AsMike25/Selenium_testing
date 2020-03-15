8ebf7f5badb3dda509f4c46e3100435115debc98
MicAss <aswin@amrita.com>
2020-03-15 21:35:11 +0530

    herokuapp updation

--- basic_cmd.py
+++ basic_cmd.py
@@ -3,7 +3,7 @@ import time
 # Opening login page by clicking login button on top right of localhost
 driver = webdriver.Firefox(executable_path='C:\Python\geckodriver.exe')
 
-driver.get("http://localhost:3000/")
+driver.get("https://lit-scrubland-01256.herokuapp.com/")
 
 time.sleep(5)
 driver.find_element_by_xpath("/html/body/div/div/nav/div/ul/li/a").click()
