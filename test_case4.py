03da178c4da49c24fd1008d7483ac4175ae980b6
MicAss <aswin@amrita.com>
2020-03-15 21:37:33 +0530

    herokuapp updation

--- test_case4.py
+++ test_case4.py
@@ -3,7 +3,7 @@ from selenium.webdriver import ActionChains
 import time
 #Check window minimize,maximize and close functionality
 driver = webdriver.Firefox(executable_path='C:\Python\geckodriver.exe')
-driver.get("http://localhost:3000/")
+driver.get("https://lit-scrubland-01256.herokuapp.com/")
 driver.find_element_by_xpath("/html/body/div/div[1]/nav/div/ul/li/a").click()
 driver.maximize_window()
 time.sleep(1)
