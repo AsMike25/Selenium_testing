d1a014960e6ab541bdcc7cee638efce7ffeafc84
MicAss <aswin@amrita.com>
2020-03-15 21:36:37 +0530

    herokuapp updation

--- tes_case5.py
+++ tes_case5.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Firefox(executable_path='C:\Python\geckodriver.exe')
-driver.get("http://localhost:3000/")
+driver.get("https://lit-scrubland-01256.herokuapp.com/")
 driver.find_element_by_xpath("/html/body/div/div[1]/nav/div/ul/li/a").click()
 time.sleep(2)
 a = driver.current_url
