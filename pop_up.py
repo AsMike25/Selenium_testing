71797e48355477a9f12846aa0e5c995111222944
MicAss <aswin@amrita.com>
2020-03-15 21:36:08 +0530

    herokuapp updation

--- pop_up.py
+++ pop_up.py
@@ -3,7 +3,7 @@ import time
 
 driver = webdriver.Firefox(executable_path='C:\Python\geckodriver.exe')
 
-driver.get("http://localhost:3000/")
+driver.get("hhttps://lit-scrubland-01256.herokuapp.com/")
 time.sleep(1)
 driver.find_element_by_xpath("/html/body/div/div/nav/div/ul/li/a").click()
 driver.find_element_by_name("user_name").send_keys("admin")
