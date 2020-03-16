5e9f4b6abe1d58479a422c8b9c0f77584bfbf092
MicAss <aswin@amrita.com>
2020-03-16 08:03:41 +0530

    herokuapp updation

--- test_case2.py
+++ test_case2.py
@@ -9,7 +9,7 @@ driver.find_element_by_name("user_name").send_keys("cse17259")
 driver.find_element_by_name("password").send_keys("qwertyuiop")
 driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div/div/div[1]/div/form/div[3]/div[1]/button").click()
 time.sleep(2)
-if(driver.current_url == "https://lit-scrubland-01256.herokuapp.com/admin"):
+if(driver.current_url == "https://lit-scrubland-01256.herokuapp.com/admin" or "https://lit-scrubland-01256.herokuapp.com/admin"):
     print("Login Successful")
 else:
     get_div = driver.find_element_by_css_selector(".text-center > div:nth-child(1) > h2:nth-child(1)").text
