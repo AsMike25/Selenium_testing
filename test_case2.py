c78fc2d9e9eb1909405c650acb6a77f5c2ce3207
MicAss <aswin@amrita.com>
2020-03-16 07:50:54 +0530

    herokuapp updation

--- test_case2.py
+++ test_case2.py
@@ -9,13 +9,14 @@ driver.find_element_by_name("user_name").send_keys("cse17212")
 driver.find_element_by_name("password").send_keys("qwertyuiop")
 driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div/div/div[1]/div/form/div[3]/div[1]/button").click()
 time.sleep(2)
-if(driver.current_url != "https://lit-scrubland-01256.herokuapp.com/admin"):
+if(driver.current_url == "https://lit-scrubland-01256.herokuapp.com/admin"):
+    print("Login Successful")
+else:
     get_div = driver.find_element_by_css_selector(".text-center > div:nth-child(1) > h2:nth-child(1)").text
     print(get_div)
     if (get_div == "No Such Roll Number Exists"):
         print("Incorrect credentials")
-else:
-    print("Login Successful")
+
 
 
 
