4347bab7c85dcadbf38f8ccde4934293b8d9206d
MicAss <aswin@amrita.com>
2020-03-16 09:10:57 +0530

    herokuapp updation

--- test_case2.py
+++ test_case2.py
@@ -6,11 +6,11 @@ driver = webdriver.Firefox(executable_path='C:\Python\geckodriver.exe')
 driver.get("https://lit-scrubland-01256.herokuapp.com/")
 driver.find_element_by_xpath("/html/body/div/div[1]/nav/div/ul/li/a").click()
 driver.find_element_by_name("user_name").send_keys("cse17259")
-driver.find_element_by_name("password").send_keys("qwertyuio")
+driver.find_element_by_name("password").send_keys("qwertyuiop")
 driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div/div/div[1]/div/form/div[3]/div[1]/button").click()
 time.sleep(2)
 print(driver.current_url)
-if (driver.current_url == "https://lit-scrubland-01256.herokuapp.com/admin"):
+if (driver.current_url == "https://lit-scrubland-01256.herokuapp.com/student"):
     print("Login Successful")
 else:
     get_div = driver.find_element_by_css_selector(".text-center > div:nth-child(1) > h2:nth-child(1)").text
